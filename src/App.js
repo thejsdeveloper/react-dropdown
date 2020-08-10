496c4289ffe999aed7c8619beb9f5ac507b5c0fb
vikas yadav <>
2020-08-10 18:55:12 +0800

    adding only one dropdown

--- src/App.js
+++ src/App.js
@@ -55,17 +55,6 @@ function App() {
   return (
     <>
       <div className="container">
-        <h1 style={{ textAlign: "center" }}>Select Countries </h1>
-        <Dropdown
-          title={title}
-          items={countries}
-          onSelection={handleSelection}
-          onSearchChange={handleSearch}
-          onAddClick={handleAdd}
-        />
-      </div>
-
-      <div className="container">
         <h1 style={{ textAlign: "center" }}>
           Select Countries (With add permission)
         </h1>
