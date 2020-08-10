3ab19530e3f297064e59bbaafce01015d7d764b2
vikas yadav <>
2020-08-10 18:17:35 +0800

    final project with hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import Dropdown from "./Dropdown";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [storedCountries, setStoredCountries] = useState([]);
+  const [title, setTitle] = useState("Select Country..");
+
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all?fields=name")
+      .then((response) => response.json())
+      .then((countries) => {
+        const mappedCountries = countries.map((country) => ({
+          value: country.name,
+          id: country.name,
+        }));
+        setStoredCountries(mappedCountries);
+        setCountries(mappedCountries);
+      })
+      .catch((error) => {
+        console.error(error);
+        return [];
+      });
+  }, [setCountries, setStoredCountries]);
+
+  function handleSelection(value) {
+    if (!!value) {
+      setTitle(value);
+    } else {
+      setTitle("Select Country..");
+    }
+  }
+
+  function handleSearch(value) {
+    if (!!value) {
+      const filteredCountries = storedCountries.filter((country) =>
+        country.value.toLowerCase().includes(value.toLowerCase())
+      );
+      setCountries(filteredCountries);
+    } else {
+      setCountries(storedCountries);
+    }
+  }
+
+  function handleAdd(country) {
+    const sortedCountries = [...storedCountries, country].sort((a, b) =>
+      a.value.toLowerCase() > b.value.toLowerCase() ? 1 : -1
+    );
+
+    setStoredCountries(sortedCountries);
+    setCountries(sortedCountries);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 style={{ textAlign: "center" }}>Select Countries </h1>
+      <Dropdown
+        title={title}
+        items={countries}
+        onSelection={handleSelection}
+        onSearchChange={handleSearch}
+        onAddClick={handleAdd}
+      />
     </div>
   );
 }
---
+++ src/Dropdown.js
@@ -0,0 +1,190 @@
+import React, { useState, useRef, useEffect } from "react";
+
+import { FaCheck, FaAngleUp, FaAngleDown, FaSearch } from "react-icons/fa";
+import { useOnClickOutside } from "./useOnClickOutside";
+import { Subject } from "rxjs";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  tap,
+} from "rxjs/operators";
+
+const Dropdown = ({
+  title = "Select Items..",
+  items,
+  limit = 5,
+  hasAddPermission = true,
+  onSelection,
+  onSearchChange,
+  onAddClick,
+}) => {
+  const [open, setOpen] = useState(false);
+  const [showAll, setShowAll] = useState(false);
+  const [selection, setSelection] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const toggle = () => setOpen(!open);
+  const ref = useRef();
+  const inputRef = useRef();
+
+  useOnClickOutside(ref, () => {
+    setSearchText("");
+    onSearchChange("");
+    setOpen(false);
+  });
+
+  useEffect(() => {
+    if (selection.length) {
+      onSelection(selection[0].value);
+    } else {
+      onSelection("");
+    }
+  }, [selection, onSelection]);
+
+  useEffect(() => {
+    inputRef.current = new Subject();
+    const subscription = inputRef.current
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        filter((input) => input.trim().length > 1),
+        tap(console.log)
+      )
+      .subscribe((value) => onSearchChange(value));
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [onSearchChange]);
+
+  function handleOnClick(item) {
+    if (!selection.some((curr) => curr.id === item.id)) {
+      setSelection([item]);
+    } else {
+      setSelection([]);
+    }
+  }
+
+  function isItemInSelection(item) {
+    return selection.some((curr) => curr.id === item.id);
+  }
+
+  function handleShowAll(isShowAll) {
+    setShowAll(isShowAll);
+  }
+
+  function handleSearch(value) {
+    setSearchText(value.trim());
+    inputRef.current.next(value);
+  }
+
+  function handleAdd() {
+    const [firstChar, ...rest] = searchText;
+    const capitalisedValue = firstChar.toUpperCase() + rest.join("");
+    const country = {
+      id: capitalisedValue,
+      value: capitalisedValue,
+    };
+
+    onAddClick(country);
+    handleOnClick(country);
+    setOpen(false);
+    setSearchText("");
+  }
+
+  return (
+    <div className="dd-wrapper" ref={ref}>
+      <div
+        className="dd-header"
+        onKeyPress={() => toggle()}
+        onClick={() => toggle()}
+      >
+        <div className="dd-header__title">
+          <p className="dd-header__title--bold">{title}</p>
+        </div>
+        <div className="dd-header__action">
+          <p>{open ? <FaAngleUp /> : <FaAngleDown />} </p>
+        </div>
+      </div>
+
+      {open && (
+        <>
+          <div className="dd-search">
+            <span className="dd-search__icon">
+              <FaSearch color="#00B0FF" />
+            </span>
+            <input
+              type="text"
+              placeholder="Filter Countries"
+              value={searchText}
+              onChange={(e) => handleSearch(e.target.value)}
+            />
+          </div>
+
+          <ul className="dd-list">
+            {!showAll
+              ? items.slice(0, limit).map((item) => (
+                  <li className="dd-list-item" key={item.id}>
+                    <button
+                      type="button"
+                      onClick={() => handleOnClick(item)}
+                      className={isItemInSelection(item) ? "selected" : ""}
+                    >
+                      <span>{item.value}</span>
+                      <span>
+                        {isItemInSelection(item) && <FaCheck color="green" />}
+                      </span>
+                    </button>
+                  </li>
+                ))
+              : items.map((item) => (
+                  <li className="dd-list-item" key={item.id}>
+                    <button
+                      type="button"
+                      onClick={() => handleOnClick(item)}
+                      className={isItemInSelection(item) ? "selected" : ""}
+                    >
+                      <span>{item.value}</span>
+                      <span>
+                        {isItemInSelection(item) && <FaCheck color="green" />}
+                      </span>
+                    </button>
+                  </li>
+                ))}
+
+            {items.length - limit > 0 && (
+              <li className="dd-list-item">
+                {!showAll ? (
+                  <button type="button" onClick={() => handleShowAll(true)}>
+                    <span>{""}</span>
+                    <span>{items.length - limit} more.. </span>
+                  </button>
+                ) : (
+                  <button type="button" onClick={() => handleShowAll(false)}>
+                    <span>{""}</span>
+                    <span>Show less</span>
+                  </button>
+                )}
+              </li>
+            )}
+
+            {items.length === 0 && hasAddPermission && !!searchText.trim() && (
+              <>
+                <li className="dd-list-item">
+                  <button type="button">
+                    <span>
+                      "<strong>{searchText}</strong>" not found{" "}
+                    </span>
+                    <span className="button__action" onClick={handleAdd}>
+                      Add & Select{" "}
+                    </span>
+                  </button>
+                </li>
+              </>
+            )}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
+export default Dropdown;
